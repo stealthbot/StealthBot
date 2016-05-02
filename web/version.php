96da84657607f37539b9abc3075029582f8cbd17
Davnit <davnit31@gmail.com>
2016-05-01 23:07:17 -0400

    Add request logging for stats

--- web/version.php
+++ web/version.php
@@ -2,6 +2,15 @@
 	// StealthBot Version Output - Quick & Dirty
 	// 2009-1006-andy
 	
+    // set this to false to disable logging
+    $enableLogging = true;
+    
+    $cv = 0;
+    $lv = 0;
+    
+    if (isset($_GET["cv"])) $cv = $_GET["cv"];
+    if (isset($_GET["lv"])) $lv = $_GET["lv"];
+    
 	$beta_build = 489;
 	$regular_build = 1;
 	$launcher_build = 0x01020000 + 1;
@@ -21,5 +30,24 @@ You can get the latest version of StealthBot at http://stealthbot.net/sb/release
 	"));
 
 	echo "$beta_build|$regular_build|$launcher_build|$regular_news|$beta_news";
+    
+    
+    // request logging, for statistical purposes - added 2016-03-22 by Pyro
+    // records the following
+    //   - date+time (in server time, currently America\Los_Angeles)
+    //   - sha1 of remote IP
+    //   - CVERSION
+    //   - launcher version
+    // CVERSION and launcher version are as reported by the bot.
+    if ($enableLogging && isset($_GET["cv"])) {
+        error_reporting(0);
+        
+        $fileName = "version.log";
+
+        if (($file = @fopen($fileName, "a")) != false) {
+            @fwrite($file, implode(" ", array(date("Y-m-d H:i:s"), sha1($_SERVER['REMOTE_ADDR']), $cv, $lv, "\r\n")));
+            @fclose($file);
+        }
+    }
 ?>
 
