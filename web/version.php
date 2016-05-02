da87b910f8d5205a4232df854e51cb8a080bf4e6
Davnit <davnit31@gmail.com>
2016-05-01 23:06:30 -0400

    Update for 489 release

--- web/version.php
+++ web/version.php
@@ -2,7 +2,7 @@
 	// StealthBot Version Output - Quick & Dirty
 	// 2009-1006-andy
 	
-	$beta_build = 487;
+	$beta_build = 489;
 	$regular_build = 1;
 	$launcher_build = 0x01020000 + 1;
 	$regular_news = str_replace("\n","\\n", trim("
@@ -16,8 +16,8 @@ Visit http://www.stealthbot.net for more information.
 	"));
 
 	$beta_news = str_replace("\n","\\n", trim("
-Visit http://www.stealthbot.net/wiki/BuildLog for Build information. Please test
-the latest beta version thoroughly as it might become the next stable release.
+It's 2016 and a new build of StealthBot has been released!
+You can get the latest version of StealthBot at http://stealthbot.net/sb/releases/
 	"));
 
 	echo "$beta_build|$regular_build|$launcher_build|$regular_news|$beta_news";
