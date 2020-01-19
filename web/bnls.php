75e743029171e421776c67c45616f503c58020ec
Carl Bennett <carl@carlbennett.me>
2020-01-19 05:25:47 -0600

    Add dynamically generated BNLS server list

--- web/bnls.php
+++ web/bnls.php
@@ -1,18 +1,27 @@
 <?php
+// Dynamically retrieve the list of online BNLS servers from BNETDocs:
+$url = 'https://bnetdocs.org/servers.json?type_id=4&status=1';
 
-echo "\n";
-// echo "hdx.jbls.org\n";
-// echo "logon.berzerkerjbls.com\n";
-echo "pyro.no-ip.biz\n";
-// echo "alendar.no-ip.org\n";
-// echo "distant.no-ip.org\n";
-// echo "jbls.idiat.com\n";
-// echo "jbls.org\n";
-echo "phix.no-ip.org\n";
-echo "bnls.mattkv.net\n";
-echo "bnls.net\n";
-//echo "ribose.no-ip.org\n";
-//echo "wjlafrance.net\n";
-//echo "7thace.no-ip.org\n";
+header('Content-Type: text/plain;charset=utf-8');
 
-?>
+function exit_here($message, $code = 500) {
+  if (function_exists('http_response_code')) {
+    http_response_code($code); // introduced in PHP 5.4
+  } else {
+    header($_SERVER['SERVER_PROTOCOL'] . ' ' . $code);
+  }
+  die($message . "\n");
+}
+
+if (!($data = file_get_contents($url))) {
+  exit_here(sprintf('Failed to download server list from: %s', $url));
+}
+
+if (!($json = json_decode($data, true))) {
+  exit_here(sprintf('JSON error #%d while decoding list', json_last_error()));
+}
+
+echo "\n"; // not sure why this is here - @carlbennett
+foreach ($json['servers'] as $server) {
+  echo $server['address'] . "\n";
+}
